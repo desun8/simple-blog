f20a468f281f25d50fefedc5b64004d5dfe8d105
Evgeniy Pchelinov <epchelinov@gmail.com>
2023-03-15 08:11:15 +0500

    init

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,13 @@
+import vue from "@vitejs/plugin-vue"
+import { PluginOption } from "vite";
+import { optimizeCssModules } from 'vite-plugin-optimize-css-modules';
+
+export const buildPlugins = (isDev: boolean): PluginOption[] => {
+  const plugins = [vue()]
+
+  if (isDev) {
+    plugins.push(optimizeCssModules())
+  }
+
+  return plugins
+}
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig, loadEnv, UserConfigExport } from "vite";
+import { buildPlugins } from "./config/build/buildPlugins";
+
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development'
+  const env = loadEnv(mode, process.cwd(), '')
+
+
+  const config: UserConfigExport = {
+    plugins: buildPlugins(isDev),
+    server: {
+      port: Number(env.DEV_SERVER_PORT)
+    }
+  }
+
+  return config
+})
