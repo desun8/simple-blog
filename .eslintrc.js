2198ec944c1548a20a0016eea7b94bba4d4e6edc
Evgeniy Pchelinov <epchelinov@gmail.com>
2023-03-18 18:36:25 +0500

    build: add prettier, fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,39 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		node: true,
-	},
-	extends: [
-		'plugin:vue/vue3-essential',
-		'xo',
-	],
-	overrides: [
-		{
-			extends: [
-				'xo-typescript',
-			],
-			parserOptions: {
-				project: ['tsconfig.json', 'tsconfig.node.json'],
-			},
-			files: [
-				'*.ts',
-				'*.tsx',
-			],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["plugin:vue/vue3-essential", "xo", "prettier"],
+  overrides: [
+    {
+      extends: ["xo-typescript"],
+      parserOptions: {
+        project: ["tsconfig.json", "tsconfig.node.json"],
+      },
+      files: ["*.ts", "*.tsx"],
 
-			rules: {
-				'@typescript-eslint/indent': 'off',
-				'@typescript-eslint/naming-convention': ['error', {
-					selector: 'variable',
-					format: ['camelCase', 'UPPER_CASE', 'StrictPascalCase'],
-				}],
-			},
-		},
-	],
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: [
-		'vue',
-	],
-	rules: {
-		'capitalized-comments': 'off',
-	},
+      rules: {
+        "@typescript-eslint/indent": "off",
+        "@typescript-eslint/naming-convention": [
+          "error",
+          {
+            selector: "variable",
+            format: ["camelCase", "UPPER_CASE", "StrictPascalCase"],
+          },
+        ],
+      },
+    },
+  ],
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["vue", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "capitalized-comments": "off",
+    "no-unused-vars": "off",
+  },
 };
