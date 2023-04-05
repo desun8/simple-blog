44d8ae469b0fc84901b5e67e345ca9e5def3b29b
Evgenii <epchelinov@gmail.com>
2023-04-05 17:42:00 +0500

    setup ci

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -1,13 +1,24 @@
 import { type CustomProjectConfig } from 'lost-pixel';
 
-export const config: CustomProjectConfig = {
-  storybookShots: {
+const getConfig = (): CustomProjectConfig => {
+  const storybookShots = {
     storybookUrl: './storybook-static',
-  },
+  };
 
-  // generateOnly: true,
-  // failOnDifference: true,
+  if (process.env.LOCAL === 'true') {
+    return {
+      generateOnly: true,
+      failOnDifference: true,
+      storybookShots,
+    };
+  }
 
-  lostPixelProjectId: 'clg26im9304bdie0e6w3whyxe',
-  apiKey: process.env.LOST_PIXEL_API_KEY,
+  // @ts-expect-error: For ci it's ok
+  return {
+    lostPixelProjectId: 'clg26im9304bdie0e6w3whyxe',
+    apiKey: process.env.LOST_PIXEL_API_KEY,
+    storybookShots,
+  };
 };
+
+export const config = getConfig();
