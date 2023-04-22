af6842ebaf2ed77baf3a572cd0ddc0cce84fdd95
Evgenii <epchelinov@gmail.com>
2023-04-22 14:51:37 +0500

    feat: add auth header to request

--- src/6_shared/lib/useFetch/useFetch.ts
+++ src/6_shared/lib/useFetch/useFetch.ts
@@ -1,7 +1,16 @@
+import { USER_LOCALSTORAGE_KEY } from '6_shared/const/localStorage';
 import { createFetch } from '@vueuse/core';
 
 export const useFetch = createFetch({
   baseUrl: import.meta.env.VITE_BASE_URL as string,
   combination: 'chain',
-  options: {},
+  options: {
+    async beforeFetch({ options }) {
+      options.headers = {
+        ...options.headers,
+        Authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) ?? '',
+      };
+      return { options };
+    },
+  },
 });
