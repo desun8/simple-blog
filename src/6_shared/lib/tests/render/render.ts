4b4adae45223e56c4f1605b715bb09d9523c97d9
Evgenii <epchelinov@gmail.com>
2023-05-13 14:37:37 +0500

    test: fix render wrapper
    Add pinia

--- src/6_shared/lib/tests/render/render.ts
+++ src/6_shared/lib/tests/render/render.ts
@@ -5,8 +5,9 @@ import {
 } from '@testing-library/vue';
 import { i18n } from '6_shared/config/i18n/i18n';
 import { router } from '1_app/providers/router';
+import { pinia } from '6_shared/config/pinia/pinia';
 
-const defaultPlugins: Plugin[] = [router, i18n];
+const defaultPlugins: Plugin[] = [pinia, router, i18n];
 
 export const render = (component: Component, configs: RenderOptions = {}) => {
   const options = { ...configs };
